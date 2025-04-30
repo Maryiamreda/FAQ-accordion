47a60c193c944baaa916cd2f615b65faf4bb7f71
Maryiamreda <maryiamreda@gmail.com>
2025-05-01 01:45:26 +0300

    make question data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,21 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 export default async function PostsPage() {
-  const { data: posts, error } = await supabase.from("faq").select("id, question, created_at");
-  if (error) return <p>Error loading posts</p>;
-  if (!posts || posts.length === 0) return <p>No posts found.</p>;
+  const { data: questions, error } = await supabase.from("faq").select("id, question, created_at");
+  if (error) return <p>Error loading </p>;
 
-  console.log("Posts:", posts, "Error:", error);
+  console.log("questions:", questions, "Error:", error);
 
   return (
     <div className="max-w-2xl mx-auto p-6">
       <h1 className="text-2xl font-bold mb-4">qs</h1>
       <ul>
-        {posts?.map((post) => (
-          <li key={post.id} className="mb-2">
-            <Link href={`/post/${post.id}`} className="text-blue-600 hover:underline">
-              {post.question}
+        {questions?.map((question) => (
+          <li key={question.id} className="mb-2">
+            <Link href={`/post/${question.id}`} className="text-blue-600 hover:underline">
+              {question.question}
             </Link>
-            <p className="text-sm text-gray-500">{new Date(post.created_at).toDateString()}</p>
+            <p className="text-sm text-gray-500">{new Date(question.created_at).toDateString()}</p>
           </li>
         ))}
       </ul>
