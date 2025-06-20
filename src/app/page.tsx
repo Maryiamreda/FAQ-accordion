f9f3768ffcf633db6129751ea9556733532b6959
Maryiamreda <maryiamreda@gmail.com>
2025-06-20 23:22:29 +0300

    install tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function PostsPage() {
   return (
     <div className="max-w-2xl mx-auto p-6">
       {faq && faq.length > 0 ? (
-        <div>
+        <div className=''>
           {faq.map((faq, index) => (
             <div key={index} className="mb-4">
               <h2 className="text-xl font-semibold">{faq.question}</h2>
