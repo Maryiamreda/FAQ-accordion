58709398aea5105828f4a436ca1fa76a3edef7f4
Maryiamreda <maryiamreda@gmail.com>
2025-06-20 23:00:43 +0300

    added data and types files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,19 @@
-import { createClient } from "@supabase/supabase-js";
-import Link from "next/link";
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+import { faq } from './data';
+
 export default async function PostsPage() {
-  const { data: questions, error } = await supabase.from("faq").select("id, question, created_at");
-  if (error) return <p>Error loading </p>;
-
-  console.log("questions:", questions, "Error:", error);
-
   return (
     <div className="max-w-2xl mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-4">qs</h1>
-      <ul>
-        {questions?.map((question) => (
-          <li key={question.id} className="mb-2">
-            <Link href={`/post/${question.id}`} className="text-blue-600 hover:underline">
-              {question.question}
-            </Link>
-            <p className="text-sm text-gray-500">{new Date(question.created_at).toDateString()}</p>
-          </li>
-        ))}
-      </ul>
+      {faq && faq.length > 0 ? (
+        <div>
+          {faq.map((faq, index) => (
+            <div key={index} className="mb-4">
+              <h2 className="text-xl font-semibold">{faq.question}</h2>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>Loading</div>
+      )}
     </div>
   );
 }
-
