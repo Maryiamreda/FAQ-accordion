12bcb70a6aeffa73198abe23d28eda153c756722
Maryiamreda <maryiamreda@gmail.com>
2025-06-21 00:18:07 +0300

    show answer functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+'use client';
+
+import { useState } from 'react';
 import { faq } from './data';
 import Image from 'next/image'
 
-export default async function PostsPage() {
+export default  function PostsPage() {
+   const [showAnswer, setShowAnswer] = useState<number | null>(null);
+
+  const toggleAnswer = (index: number) => {
+    setShowAnswer(showAnswer === index ? null : index);
+  };
+
   return (
     <div className="bg-white p-11  rounded-xl  ">
       <div className='flex flex-row gap-4 mb-7'>
@@ -12,8 +21,23 @@ export default async function PostsPage() {
       {faq && faq.length > 0 ? (
         <div className='flex flex-col text-start gap-5 '>
           {faq.map((faq, index) => (
-            <div key={index} className="border-b-[0.01px] border-b-light-grayish-blue pb-5">
-              <h1 className="">{faq.question}</h1>
+            <div key={index} className="border-b-[0.01px] border-b-light-grayish-blue pb-5 ">
+              <div className='flex justify-between' >
+              <h1 className="font-bold text-sm w-90">{faq.question}</h1>
+
+                {showAnswer==index ?
+              <Image src={'/images/icon-minus.svg'} alt={"ICON"} width="22" height="22" className='ml-6' onClick={() => toggleAnswer(index)} />
+
+               :
+                            <Image src={'/images/icon-plus.svg'} alt={"ICON"} width="22" height="22" className='ml-6' onClick={() => toggleAnswer(index)}/>
+
+              }
+              </div>
+           
+                          {showAnswer==index && <h2 className='text-sm w-90'>{faq.answer}</h2>}
+
+
+
             </div>
           ))}
         </div>
