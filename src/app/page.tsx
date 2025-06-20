195f7145331f0c7dcc636b1cf21230f75267015f
Maryiamreda <maryiamreda@gmail.com>
2025-06-20 23:53:33 +0300

    assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { faq } from './data';
+import Image from 'next/image'
 
 export default async function PostsPage() {
   return (
-    <div className="max-w-2xl mx-auto p-6">
+    <div className="bg-white ">
+      <div className='flex flex-row gap-4'>
+       <Image src={'/images/icon-star.svg'} alt={"ICON"} width="27" height="27" />
+
+        <h1 className=' text-4xl font-bold text-[hsl(292, 42%, 14%)]'>FAQs</h1>
+      </div>
       {faq && faq.length > 0 ? (
-        <div className=''>
+        <div className='flex flex-col text-start gap-10 '>
           {faq.map((faq, index) => (
-            <div key={index} className="mb-4">
-              <h2 className="text-xl font-semibold">{faq.question}</h2>
+            <div key={index} className="border-b-[0.01px] border-b-light-grayish-blue">
+              <h1 className="">{faq.question}</h1>
             </div>
           ))}
         </div>
