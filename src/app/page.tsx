f6fd8cd3ae063cfc6ee570da0a8635290eba6170
Maryiamreda <maryiamreda@gmail.com>
2025-06-21 00:33:24 +0300

    responsive background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,24 +19,24 @@ export default  function PostsPage() {
         <h1 className=' text-4xl font-bold text-[hsl(292, 42%, 14%)]'>FAQs</h1>
       </div>
       {faq && faq.length > 0 ? (
+
         <div className='flex flex-col text-start gap-5 '>
+          
           {faq.map((faq, index) => (
             <div key={index} className="border-b-[0.01px] border-b-light-grayish-blue pb-5 ">
               <div className='flex justify-between' >
-              <h1 className="font-bold text-sm w-90">{faq.question}</h1>
+              <h1 className="font-bold text-sm md:w-90">{faq.question}</h1>
 
-                {showAnswer==index ?
-              <Image src={'/images/icon-minus.svg'} alt={"ICON"} width="22" height="22" className='ml-6' onClick={() => toggleAnswer(index)} />
+              {showAnswer==index ?
+                 <Image src={'/images/icon-minus.svg'} alt={"ICON"}  width="22" height="22" className='ml-6 cursor-pointer' onClick={() => toggleAnswer(index)} />
 
-               :
-                            <Image src={'/images/icon-plus.svg'} alt={"ICON"} width="22" height="22" className='ml-6' onClick={() => toggleAnswer(index)}/>
+                 :
+                 <Image src={'/images/icon-plus.svg'} alt={"ICON"} width="22" height="22" className='ml-6 cursor-pointer' onClick={() => toggleAnswer(index)}/>
 
               }
               </div>
            
-                          {showAnswer==index && <h2 className='text-sm w-90'>{faq.answer}</h2>}
-
-
+              {showAnswer==index && <h2 className='text-sm font-light md:w-90'>{faq.answer}</h2>}
 
             </div>
           ))}
